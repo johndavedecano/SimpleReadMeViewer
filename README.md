2f30fdad22cf1f269a7ea16c437cd9604d905d19
Dave Decano <johndavedecano@gmail.com>
2014-10-09 11:53:46 +0800

    Third COmmit

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ View your .md files on a fly.
 3. PHP5+
 
 ##Installation
-1. Clone the repository
+1. Clone the repository **git clone https://github.com/johndavedecano/SimpleReadMeViewer.git**
 2. Assuming you already have bower and node.js installed in your system, using your commandline run **composer install** next **bower install**
 3. Test it in your local server  php -S localhost:8000 
 
--- index.php
+++ index.php
@@ -5,10 +5,23 @@
         <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
         <script type="text/javascript" src="app.js"></script>
     </head>
-    <body>
+    <body onload="view()">
         <div class="row">
             <div class="col-lg-4">
-                <textarea id="markdown" onchange="view()" class="btn-block" style="min-height: 100%;"></textarea>
+                <textarea id="markdown" onchange="view()" class="btn-block" style="min-height: 100%; font-size:11px; color:white; background: black; padding:5px; font-family: Courier;">
+#Readme Viewer
+View your .md files on a fly.
+
+##Requirements
+1. Node.js and Bower
+2. Composer
+3. PHP5+
+
+##Installation
+1. Clone the repository **git clone https://github.com/johndavedecano/SimpleReadMeViewer.git**
+2. Assuming you already have bower and node.js installed in your system, using your commandline run **composer install** next **bower install**
+3. Test it in your local server  php -S localhost:8000 
+                </textarea>
             </div>
             <div class="col-lg-6" id="contents">
                 
